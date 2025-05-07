076c010a145704e67ca87a81000087a72f49f5bb
samuwa95 <samualwa@edu.hac.ac.il>
2025-05-07 20:19:59 +0300

    we just fnshied the ex

--- ex2/src/App.js
+++ ex2/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
+import Favorites from './pages/Favorites';
 import NavBar from './components/NavBar';
 
 //קומפוננטה ראשית
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
+          <Route path="/favorites" element={<Favorites />} />
         </Routes>
       </div>
     </Router>
